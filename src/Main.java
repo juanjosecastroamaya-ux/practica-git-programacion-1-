17267cd73077c8aa4e5407ac1590bb9bbed14b15
juanCA <juanjosecastroamaya@gamil.com>
2025-08-28 12:37:58 -0500

    comandos

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("git status");
+        System.out.println("gti add");
+        System.out.println("git commit -m");
+        System.out.println("git push origin");
 
     }
 }
